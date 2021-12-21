cee3ffb6b1de31f8ed67187bdf5da1f2a9c43ac3
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2021-12-21 14:13:52 +0700

    first commit

---
+++ tinhtong.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a, b;
+    cout << "nhap vao gia tri a = ";
+    cin >> a;
+    cout << "\nnhap vao gia tri b = ";
+    cin >> b;
+
+    cout << "\nTong a + b = " << a + b;
+    return 0;
+
+
+
+}
---
+++ variable_constant.c
@@ -0,0 +1,11 @@
+
+
+#include<stdio.h>
+
+int main() {
+    // int color = 1;
+    const int color = 5;
+
+    printf ("color = %d", color);
+    return 0;
+}
