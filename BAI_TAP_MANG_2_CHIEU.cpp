6f54ad21c920ca7c40896a5231742abad539294c
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-05-04 09:50:15 +0700

    C 04/05/2022

--- BAI_TAP_MANG_2_CHIEU.cpp
+++ BAI_TAP_MANG_2_CHIEU.cpp
@@ -1,18 +1,21 @@
 
 // mảng 2 chiều: 72DCHT21
 /*
-    a.  in mảng ra màn hình
-    b. tinh tổng các phần tử lẻ
-    c.  Tính TBC các só chia hết cho 3 và tận cùng là 6
-    d. Tính tổng các phần tử nằm trên đường chéo chính
-    e. Tìm phần tử nhỏ nhất
 
-*/
+
+    e. đến cố nguyên tố trong mảng
+    f. sắp sếp mảng tăng dần hoạc giâm dần
+    g. đến số lần xuất hiện của số n bất kỳ trong mảng
+    h. hóa phần tử trong mảng
+
+
+*/  
 
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 void Print_Array(int m, int n, int arr[][100]) {
     printf("Elements Array 2: \n");
@@ -24,13 +27,11 @@ void Print_Array(int m, int n, int arr[][100]) {
     }
 }
 
-int Sum_Even_Elements_Array(int m, int n, int arr[][100]) {
+int Sum_Elements_Array(int m, int n, int arr[][100]) {
     int sum = 0;
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
-            if (arr[i][j] % 2 != 0) {
-                sum += arr[i][j];
-            }
+            sum += arr[i][j];
         }
     }
     return sum;
@@ -45,24 +46,10 @@ int Tong_Phan_Tu_Tren_Duong_Cheo_Chinh(int m, int n, int arr[][100]) {
             }
         }
     }
-
     return sum;
 }
 
-float TBC_Array(int m, int n, int arr[][100]) {
-    float TBC = 0, count = 0;
-    for(int i=0; i<m; i++) {
-        for(int j=0; j<n; j++) {
-            if (arr[i][j] % 3 == 0 && (arr[i][j] % 10 == 6)) {
-                TBC += arr[i][j];
-                count++;
-            }
-        }
-    }
-    TBC /= count;
-    return TBC;
-}
-
+// tìm Min của mảng
 int Elements_Min_Array(int m, int n, int arr[][100]) {
     int min = arr[0][0];
     for(int i=0; i<m; i++) {
@@ -74,6 +61,137 @@ int Elements_Min_Array(int m, int n, int arr[][100]) {
     }
     return min;
 }
+
+// tìm max của mảng
+int Elements_Max_Array (int m, int n, int arr[][100]) {
+    int max = arr[0][0];
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            if (max < arr[i][j]) {
+                max = arr[i][j];
+            }
+        }
+    }
+    return max;
+}
+
+
+
+//e. đến số nguyên tố trong mảng
+bool nt(int a) {
+    for (int i=2; i<=sqrt(a); i++) {
+        if (a % i == 0) {
+           return false;
+        }
+    }
+    return a > 1;
+}
+
+int CountPrime(int m, int n, int arr[][100]) {
+    int count = 0;
+    for(int i=0; i<m; i++) {
+        for(int j=0; j< n; j++) {
+            if (nt(arr[i][j])) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// e. đếm số nguyên tố trong mảng
+int CountPrime_Array (int m, int m, int arr[][100]) {
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j <n; j++) {
+            if (arr[i][j] % 2 != 0) {
+                count++;
+            }
+        }
+    }
+
+
+
+    return count;
+}
+
+
+// sắp xếp mảng 2 chiều tăng dần theo hàng
+void Swap_Tang_BubbleSort(int m, int n, int arr[][100]) {
+
+    for(int k=0; k<m; k++) {
+        for(int i=0; i<n - 1; i++) {
+            for(int j= n - 1; j > i; j--) {
+                if (arr[k][j] < arr[k][j-1]) {
+                    int temp = arr[k][j];
+                    arr[k][j] = arr[k][j-1];
+                    arr[k][j-1] = temp;
+                }
+            }
+        } 
+    }
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+
+// sắp xếp mảng tăng dần
+int Swap_Array_2_Tang (int m, int n, int arr[][100]){
+    int p = m * n;
+    for(int i=0; i<m-1; i++) {
+        for(int j = i+ 1; j<n ; j++) {
+            if (arr[i/n][i%n] > arr[j/n][j%n]) {
+                int temp = arr[i/n][i%n];
+                arr[i/n][i%n] = arr[j/n][j%n];
+                arr[j/n][j%n] = temp;
+            }
+        }
+    }
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+// số lần xuất hiện của các phần tử trong mảng
+// void Count_Element_Array () {
+//     in brr[100][100], count;
+//     for(int i=0; i<m; i++) {
+//         for(int j=0; j<n; j++) {
+//             brr[i][j] = 1;
+//         }
+//     }
+    
+//     for (int i=0; i<m; i++) {
+//         for(int j=0; j<n; j++) {
+//             if (brr[i][j]) {
+//                 count = 1;
+//                 for (int j=i+1; j<n; j++) {
+//                     if (arr[i] == arr[j]) {
+//                         count++;
+//                         brr[j] = 0;
+//                     }
+//                 }
+//                 printf ("\nGia tri %d xuat hien %d lan!", arr[i], count);
+//             }
+//         }
+//     }
+// }
+
 
 int main() {
     int m, n;
@@ -88,10 +206,20 @@ int main() {
     }
 
     Print_Array(m, n, arr);
-    printf("\nSum Elements Even Array:\t %d", Sum_Even_Elements_Array(m, n, arr));
-    printf("\nTBC digits:\t %.3f", TBC_Array(m, n, arr));
+    printf("\nSum Elements Even Array:\t %d", Sum_Elements_Array(m, n, arr));
     printf("\nElements Min Array:\t %d", Elements_Min_Array(m, n, arr));
+    printf("\nElements Max Array:\t %d", Elements_Max_Array(m, n, arr));
     printf("\nTong Cac Phan Tu Tren Duong Cheo Chinh:\t %d", Tong_Phan_Tu_Tren_Duong_Cheo_Chinh(m, n, arr));
+
+
+    printf("\nCountPrime: \t%d\n", CountPrime(m, n, arr));
+
+    printf("Mang tang dan theo hang: \n");
+    Swap_Tang_BubbleSort(m, n, arr);
+
+    printf("Mang sap xep tang dan: \n");
+    Swap_Array_2_Tang(m, n, arr);
+
 
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+void Count_Element_Array () {
+//     for (int i=0; i<m; i++) {
+//         for(int j=0; j<n; j++) {
+//             if (brr[i]) {
+//                 count = 1;
+//                 for (int j=i+1; j<n; j++) {
+//                     if (arr[i] == arr[j]) {
+//                         count++;
+//                         brr[j] = 0;
+//                     }
+//                 }
+//                 printf ("\nGia tri %d xuat hien %d lan!", arr[i], count);
+//             }
+//         }
+        
+//     }
+// }
